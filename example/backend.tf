80cc8c1da30df57836e6e18db55596565149c441
mahamud <mahmud.fahim1991@gmail.com>
2024-04-06 23:44:41 -0400

    update access token

--- example/backend.tf
+++ example/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "http" {
     address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/default_project/workspaces/harnessws/terraform-backend?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
     username = "harness"
-    password = "wnqWOUNaRa2HMLIZyS_56A"
+    password = "pat.wnqWOUNaRa2HMLIZyS_56A.66120dc7d70c81716758c6f7.hiH8yL0xXojVXVLRi6Uu"
     lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/default_project/workspaces/harnessws/terraform-backend/lock?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
     lock_method = "POST"
     unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/default_project/workspaces/harnessws/terraform-backend/lock?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
