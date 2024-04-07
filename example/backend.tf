4e4e57fef11ce154d3b020477a76b77025612831
mahamudulc <91023063+mahamudulc@users.noreply.github.com>
2024-04-07 14:32:36 -0400

    Update backend.tf

--- example/backend.tf
+++ example/backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "http" {
-    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/default_project/workspaces/harnessws/terraform-backend?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
+    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/HarnessAzureTerraform/workspaces/AzureHarnessTest/terraform-backend?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
     username = "harness"
     password = "pat.wnqWOUNaRa2HMLIZyS_56A.66120dc7d70c81716758c6f7.hiH8yL0xXojVXVLRi6Uu"
-    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/default_project/workspaces/harnessws/terraform-backend/lock?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
+    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/HarnessAzureTerraform/workspaces/AzureHarnessTest/terraform-backend/lock?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
     lock_method = "POST"
-    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/default_project/workspaces/harnessws/terraform-backend/lock?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
+    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/HarnessAzureTerraform/workspaces/AzureHarnessTest/terraform-backend/lock?accountIdentifier=wnqWOUNaRa2HMLIZyS_56A"
     unlock_method = "DELETE"
   }
 }
