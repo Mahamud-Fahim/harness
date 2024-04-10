856ba907daeee1f1cfeaedfac61b883a5f1ba104
mahamudulc <91023063+mahamudulc@users.noreply.github.com>
2024-04-10 15:55:37 -0400

    Update versions.tf

--- example/versions.tf
+++ example/versions.tf
@@ -12,11 +12,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-    subscription_id = "ade5225b-32d1-4280-aadb-0e7f7779a936"
-    tenant_id = "ee055aa6-611f-4dfd-8e7f-5969354341c9"
-    client_id = "fb5b90d5-1b71-4b73-ba79-fbaa2a09d27f"
-    client_secret = "Xwy8Q~HOTT0JCfRH15zIgwUz1oM~WkVaG1SzWdsT"
-    
-    
-  
 }
