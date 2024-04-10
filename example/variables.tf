6dc2aba4d40d6391c082d512b083fd7677b7d7ed
mahamudulc <91023063+mahamudulc@users.noreply.github.com>
2024-04-10 16:04:55 -0400

    Update variables.tf

--- example/variables.tf
+++ example/variables.tf
@@ -9,3 +9,10 @@ variable "resource_group_name" {
   default = "harness-test-rg"
 }
 
+variable "subscription_id" {}
+
+variable "tenant_id" {}
+
+variable "client_id" {} 
+
+variable "client_secret" {}
